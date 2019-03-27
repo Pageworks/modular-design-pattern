e12321b6558351db7b10e88bfad50c43f3e6e735
Kyle Andrews <kylea@page.works>
2019-03-27 06:28:36 -0400

    v0.0.2 - Updates Env and App classes

--- app/typescript/App.ts
+++ app/typescript/App.ts
@@ -8,9 +8,28 @@ export default class App{
 
     /**
      * Called when the class first loads.
+     * Use this method to launch any additional classes/packages.
      */
     private init(): void{
+        new Env();
         new Pjax({ debug: Env.isDebug });
+
+        document.addEventListener('pjax:complete', this.handlePageLoad);
+    }
+
+    /**
+     * Called when Pjax fires it's custom `pjax:complete` event on the `document`.
+     */
+    private handlePageLoad:EventListener = ()=>{
+        this.reinit();
+    }
+
+    /**
+     * Called when a new page is loaded via Pjax.
+     * Any classes/packages that need to manage their DOM hooks should be told to reinit.
+     */
+    public reinit():void{
+
     }
 }
 
--- app/typescript/env.ts
+++ app/typescript/env.ts
@@ -1,20 +1,30 @@
 export default class Env {
-    
+
     public static PJAX_CONTAINER:string = '.js-pjax';
     public static SCROLL_TRIGGER:number = 100;
     public static APP_NAME:string       = 'REPLACE_ME';
-    public static isDebug:boolean       = true;
+    public static isDebug:boolean       = false;
 
-    public static EASING:EasingObject   = {
-                                            ease: 'cubicBezier(0.4, 0.0, 0.2, 1)',
-                                            in: 'cubicBezier(0.0, 0.0, 0.2, 1)',
-                                            out: 'cubicBezier(0.4, 0.0, 1, 1)',
-                                            sharp: 'cubicBezier(0.4, 0.0, 0.6, 1)'
-                                        };
-    
+    public static EASING:IEasingObject   = {
+        ease: 'cubicBezier(0.4, 0.0, 0.2, 1)',
+        in: 'cubicBezier(0.0, 0.0, 0.2, 1)',
+        out: 'cubicBezier(0.4, 0.0, 1, 1)',
+        sharp: 'cubicBezier(0.4, 0.0, 0.6, 1)'
+    };
 
-    constructor(){}
 
+    constructor(){
+        // Check if the hostname is set to `.local`
+        if(window.location.hostname.match(/.local/gi)){
+            // If on a local environment enable debug
+            Env.setDebug(true);
+        }
+    }
+
+    /**
+     * Sets the static `Env.isDebug` status to the provided parameter.
+     * @param status - `boolean`
+     */
     public static setDebug(status:boolean): void{
         Env.isDebug = status;
     }
