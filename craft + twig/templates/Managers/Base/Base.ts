16f4abb4126de7e84df27403ebff48465b9b362e
Kyle Andrews <kylea@page.works>
2019-04-23 08:19:11 -0400

    Update the basic Manager example

---
+++ craft + twig/templates/Managers/Base/Base.ts
@@ -0,0 +1,40 @@
+import { Pjax } from '@codewithkyle/pjax';
+import { ModuleManager } from '@codewithkyle/ModuleManager';
+
+class Base{
+
+    constructor(){
+        console.log('The Manager has started their shift');
+        this.init();
+    }
+
+    /**
+     * Called when Pjax fires it's custom `pjax:complete` event on the `document`.
+     */
+    private handlePageLoad:EventListener = ()=>{
+        this.reinit();
+    }
+
+    private init():void{
+        // Start Pjax
+        new Pjax({ debug: true });
+
+        new ModuleManager();
+
+        // Listen for a successful page transition event
+        document.addEventListener('pjax:complete', this.handlePageLoad);
+    }
+
+    /**
+     * Called when a new page is loaded via Pjax.
+     * Any classes/packages that need to manage their DOM hooks should be told to reinit.
+     */
+    public reinit():void{
+        ModuleManager.update();
+    }
+}
+
+// IIFE used to start the Manager class
+(()=>{
+    new Base();
+})();
