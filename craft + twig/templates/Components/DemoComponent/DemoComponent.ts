fa9e8189f0ae601b84e914477ac5cd93399b33bb
Kyle Andrews <kylea@page.works>
2019-04-23 07:57:01 -0400

    Updates the DemoComponent demo to showcase assigning functionality to objects within a component

--- craft + twig/templates/Components/DemoComponent/DemoComponent.ts
+++ craft + twig/templates/Components/DemoComponent/DemoComponent.ts
@@ -1,21 +1,44 @@
+import { BaseComponent } from 'BaseComponent';
+
 export class DemoComponent extends BaseComponent {
     
     public static String COMPONENT_NAME = "DemoComponent";
     
+    private _cards: Array<HTMLElement>;
+
     constructor(el:HTMLElement, uuid:String, manager:ManagerClass){
         super(el, uuid, manager);
+        this._cards = Array.from(this.el.querySelectorAll('.js-card'));
     };
 
-    private changeColor:EventListener = ()=>{
-        console.log(`Creating a new ${ DemoComponent.MODULE_NAME } module`);
-        this.el.style.backgroundColor = 'rgb(66, 134, 244)';
+    /**
+     * Called when the `click` event fires on a card.
+     */
+    private changeColor:EventListener = (e:Event)=>{
+        const target = <HTMLElement>e.currentTarget;
+        target.style.backgroundColor = 'rgb(66, 134, 244)';
+    }
+
+    /**
+     * Called when the `click` event fires on the share button.
+     */
+    private showSharingMenu:EventListener = (e:Event)=>{
+        const target = <HTMLButtonElement>e.currentTarget;
+        
+        // Social media sharing menu logic
     }
 
     /**
      * Called when the module is created.
      */
     init():void{
-        this.el.addEventListener('click', this.changeColor);
+        console.log(`Creating a new ${ DemoComponent.MODULE_NAME } module`);
+        
+        this._cards.forEach((card)=>{
+            card.addEventListener('click', this.changeColor);
+            const shareButton = card.querySelector('.js-share-button');
+            shareButton.addEventListener('click', this.showSharingMenu);
+        });
     }
 
     /**
